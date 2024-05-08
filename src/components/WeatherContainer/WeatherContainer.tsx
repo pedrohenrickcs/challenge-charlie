237a3b6998779a510ccb31bf2890642b99c5fd2f
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-05-08 15:38:50 -0300

    style: 💄 styling WeatherCOntainer

--- src/components/WeatherContainer/WeatherContainer.tsx
+++ src/components/WeatherContainer/WeatherContainer.tsx
@@ -1,8 +1,18 @@
 const WeatherContainer = () => {
   return (
-    <div className="flex flex-row justify-around py-16">
-      <div>Icon</div>
-      <div>WeatherContainer</div>
+    <div className="flex flex-row justify-around pt-16 pb-32 bg-amber-400 bg-opacity-50">
+      <div className="container flex justify-center items-center">Icon</div>
+      <div className="container  text-textSecondary">
+        <div>
+          <p className="py-4">HOJE</p>
+          <p className="pb-4">32</p>
+        </div>
+
+        <p className="py-4">Ensolarado</p>
+        <p>Vento? NO 6.4Kmh</p>
+        <p>Humidade: 78%</p>
+        <p>Pressão: 1003hPA</p>
+      </div>
     </div>
   )
 }
