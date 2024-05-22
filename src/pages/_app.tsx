dc71dab43f7f122383230fea8e18977a4e85fb40
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-05-22 15:15:10 -0300

    fix: 🐛 adjust searchBar && reorder components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/app/layout'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
+import { Head } from 'next/document'
 
 function App({ Component, pageProps }: AppProps) {
   return (
